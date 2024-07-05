bdb9c45c2581043bee56257494d7f4c1914937ae
afadel151 <af8356207@gmail.com>
2024-07-05 21:40:58 +0200

    landing page , created product/card/category models and migrations

--- shopper/database/migrations/2024_07_05_193759_create_cards_table.php
+++ shopper/database/migrations/2024_07_05_193759_create_cards_table.php
@@ -11,14 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('cards', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->unsignedInteger('price')->default(0);
-            $table->unsignedInteger('old_price')->default(0);
-            $table->unsignedInteger('rating')->default(0);
-            $table->string('picture')->nullable();
-            //category id 
             $table->timestamps();
         });
     }
@@ -28,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('cards');
     }
 };
--- shopper/resources/js/Pages/Landing.vue
+++ shopper/resources/js/Pages/Landing.vue
@@ -3,20 +3,32 @@ import Button from "primevue/button";
 import IconField from "primevue/iconfield";
 import InputIcon from "primevue/inputicon";
 import InputText from "primevue/inputtext";
+import Rating from 'primevue/rating';
+import { ref } from 'vue';
+
+const value = ref(4);
 </script>
 
 <template>
   <div class="w-screen montserrat">
-    <div class="h-[888px] w-full">
+    <div class="h-[888px] relative w-full">
       <div class="w-full px-20 h-[63px] mt-6 flex justify-between items-center">
-        <img src="/svg/shopper_logo.svg"  alt="" />
+        <img src="/svg/shopper_logo.svg" alt="" />
         <div class="flex gap-8 items-center">
           <IconField>
             <InputIcon class="pi pi-search" />
             <InputText class="h-[60px] w-[347px]" placeholder="Search" />
           </IconField>
-          <Button class="w-[247px] h-[60px]" label="Register" />
-          <Button  class="w-[247px] h-[60px]" label="Sign in" />
+          <Button
+            class="w-[247px] h-[60px]"
+            icon="pi pi-shopping-cart"
+            label="0 items added"
+          />
+          <Button
+            class="w-[247px] h-[60px]"
+            icon="pi pi-user"
+            label="Login or Sign up"
+          />
         </div>
       </div>
       <div
@@ -56,14 +68,153 @@ import InputText from "primevue/inputtext";
           </div>
         </div>
         <div
-          class="fixed top-[767px] right-[761px] flex flex-col justify-center items-center font-[500]"
+          class="absolute top-[710px] right-[761px] flex flex-col justify-center items-center font-[500]"
         >
           <img src="/svg/playvideo.svg" alt="" />
           <p>Play video</p>
         </div>
-        <img class="fixed top-[323px] right-[628px]"  src="/svg/star.svg" width="53px" alt="">
-        <img class="fixed bottom-[66px] right-[151px]"  src="/svg/star.svg" width="53px" alt="">
+        <img
+          class="absolute top-[250px] right-[628px]"
+          src="/svg/star.svg"
+          width="53px"
+          alt=""
+        />
+        <img
+          class="absolute bottom-[180px] right-[151px]"
+          src="/svg/star.svg"
+          width="53px"
+          alt=""
+        />
       </div>
+    </div>
+    <div class="h-[680px] flex relative bg-[#845EC2]">
+      <div
+        class="w-7/12 flex flex-col justify-between items-center "
+      >
+        <p class="text-[70px] font-[800] mt-20 text-white outline-2 stroke-slate-950">Apple wireless Airpod</p>
+        <div class="w-full flex gap-[32px] absolute top-40 justify-center items-end h-[470px]">
+          <div
+            class="bg-cover w-[189px] h-[331px] bg-center rounded-t-full rounded-b-full"
+            style="
+              background-image: url(/img/3a1ce3f43d18044f08f9b1d7ba5230bf.png);
+            "
+          ></div>
+          <div
+            class="bg-cover place-self-start w-[189px] h-[331px] bg-center rounded-t-full rounded-b-full"
+            style="
+              background-image: url(/img/38f276464389dfe88e6fd15e032f9a60.png);
+            "
+          ></div>
+          <div
+            class="bg-cover w-[189px] h-[331px] bg-center rounded-t-full rounded-b-full"
+            style="
+              background-image: url(/img/16a33b1a000578efa171337ae6a6e018.png);
+            "
+          ></div>
+        </div>
+        <p class="text-[40px] pb-5 text-white">Price : $49.99</p>
+      </div>
+      <div class="w-4/12 text-white flex flex-col justify-center items-start gap-10 ">
+        <p class="text-[50px] font-[700] w-[600px]">Our Featured products</p>
+        <p class="font-[500] text-[21px] w-[500px]">Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a product</p>
+        <div class="w-full flex justify-around items-center">
+          <Button
+            class="w-[263px] h-[70px]"
+            icon="pi pi-shopping-cart"
+            label="Add to card"
+          />
+          <Button
+            class="w-[247px] h-[60px]"
+            severity="secondary"
+            icon="pi pi-user"
+            label="View more"
+          />
+        </div>
+      </div>
+    </div>
+    <div class=" h-[829px] mt-20 w-full ">
+        <p class="text-center text-[44px] font-[600]">Top sellers</p>
+        <div class="w-full flex text-[26px] space-x-5 mt-5 font-[600] justify-center items-center">
+            <p class="bg-[#10B981]  rounded-full p-8 text-white  ">Top Picks</p>
+            <p>Watches</p>
+        </div>
+        <div class="grid h-[500px] mt-20 grid-cols-3 gap-16 px-40">
+          <div class="h-[600px]">
+            <div class="bg-[#ECFEF4] p-10 h-full flex flex-col justify-center items-center gap-4   rounded-xl">
+              <img src="/img/258d2895eaafe1948111390bead0b9ee.png" class="h-1/2" alt="">
+              <div class="h-1/2 bg-gray-100 rounded-3xl p-12 flex flex-col justify-around items-start  w-full ">
+                <p class="text-[30px] font-[500]">Boat Rockerz 333</p>
+                <Rating v-model="value"  readonly />
+                <p class="text-[21px] font-[500]">$55</p>
+              </div>
+            </div>
+            <Button class="w-full mt-8 h-16" label="Add to card"  />
+          </div>
+          <div class="h-[600px]">
+            <div class="bg-[#ECFEF4] p-10 h-full flex flex-col justify-center items-center gap-4   rounded-xl">
+              <img src="/img/932316f9614920ca6f56ae8aa5e219f7.png" class="h-1/2" alt="">
+              <div class="h-1/2 bg-gray-100 rounded-3xl p-12 flex flex-col justify-around items-start  w-full ">
+                <p class="text-[30px] font-[500]">Boat Rockerz 333</p>
+                <Rating v-model="value"  readonly />
+                <p class="text-[21px] font-[500]">$55</p>
+              </div>
+            </div>
+            <Button class="w-full mt-8 h-16" label="Add to card"  />
+          </div>
+          <div class="h-[600px]">
+            <div class="bg-[#ECFEF4] p-10 h-full flex flex-col justify-center items-center gap-4   rounded-xl">
+              <img src="/img/aba819e6ef43e086604c73efc97bdfde.png" class="h-1/2" alt="">
+              <div class="h-1/2 bg-gray-100 rounded-3xl p-12 flex flex-col justify-around items-start  w-full ">
+                <p class="text-[30px] font-[500]">Boat Rockerz 333</p>
+                <Rating v-model="value"  readonly />
+                <p class="text-[21px] font-[500]">$55</p>
+              </div>
+            </div>
+            <Button class="w-full mt-8 h-16" label="Add to card"  />
+          </div>
+        </div>
+    </div>
+    <div class=" h-[829px] mt-40 w-full ">
+        <p class="text-center text-[44px] font-[600]">Top sellers</p>
+        <div class="w-full flex text-[26px] space-x-5 mt-5 font-[600] justify-center items-center">
+            <p class="bg-[#10B981]  rounded-full p-8 text-white  ">Top Picks</p>
+            <p>Watches</p>
+        </div>
+        <div class="grid h-[500px] mt-20 grid-cols-3 gap-16 px-40">
+          <div class="h-[600px]">
+            <div class="bg-[#ECFEF4] p-10 h-full flex flex-col justify-center items-center gap-4   rounded-xl">
+              <img src="/img/258d2895eaafe1948111390bead0b9ee.png" class="h-1/2" alt="">
+              <div class="h-1/2 bg-gray-100 rounded-3xl p-12 flex flex-col justify-around items-start  w-full ">
+                <p class="text-[30px] font-[500]">Boat Rockerz 333</p>
+                <Rating v-model="value"  readonly />
+                <p class="text-[21px] font-[500]">$55</p>
+              </div>
+            </div>
+            <Button class="w-full mt-8 h-16" label="Add to card"  />
+          </div>
+          <div class="h-[600px]">
+            <div class="bg-[#ECFEF4] p-10 h-full flex flex-col justify-center items-center gap-4   rounded-xl">
+              <img src="/img/932316f9614920ca6f56ae8aa5e219f7.png" class="h-1/2" alt="">
+              <div class="h-1/2 bg-gray-100 rounded-3xl p-12 flex flex-col justify-around items-start  w-full ">
+                <p class="text-[30px] font-[500]">Boat Rockerz 333</p>
+                <Rating v-model="value"  readonly />
+                <p class="text-[21px] font-[500]">$55</p>
+              </div>
+            </div>
+            <Button class="w-full mt-8 h-16" label="Add to card"  />
+          </div>
+          <div class="h-[600px]">
+            <div class="bg-[#ECFEF4] p-10 h-full flex flex-col justify-center items-center gap-4   rounded-xl">
+              <img src="/img/aba819e6ef43e086604c73efc97bdfde.png" class="h-1/2" alt="">
+              <div class="h-1/2 bg-gray-100 rounded-3xl p-12 flex flex-col justify-around items-start  w-full ">
+                <p class="text-[30px] font-[500]">Boat Rockerz 333</p>
+                <Rating v-model="value"  readonly />
+                <p class="text-[21px] font-[500]">$55</p>
+              </div>
+            </div>
+            <Button class="w-full mt-8 h-16" label="Add to card"  />
+          </div>
+        </div>
     </div>
   </div>
 </template>
@@ -75,3 +226,4 @@ import InputText from "primevue/inputtext";
   font-family: "Montserrat", sans-serif;
 }
 </style>
+
